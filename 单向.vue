9cf99be6c4f20bfb27e1539a378ca59970497c6e
usede <519802146@163.com>
2018-08-19 16:30:23 +0800

    单向

---
+++ 单向.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input v-model="msg" />
+    <input v-bind:value="msg" v-on:input="msg = $event.target.value" />
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  watch: {
+  },
+  methods: {
+  }
+}
+</script>
